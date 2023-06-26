af543b65ceb784edd06ab97ddbae43ec3a9d5d96
ketanprakash <ketanprakash001@gmail.com>
2023-06-26 18:51:29 +0530

    Created a very basic version of the app using tutorial. This converts a video to a GIF

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,41 @@
+import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
+import { useEffect, useState } from "react";
+const ffmpeg = createFFmpeg({ log: true });
+
 export default function App() {
-  return <div className="App">
-    <h1>Video Converter</h1>
-  </div>;
+  const [ready, setReady] = useState(false);
+  const [video, setVideo] = useState();
+  const [gif, setGif] = useState();
+  useEffect(() => {
+    load();
+  }, []);
+
+  const load = async () => {
+    await ffmpeg.load();
+    setReady(true);
+  };
+
+  const convertToGif = async () => {
+    ffmpeg.FS("writeFile", "src.mp4", await fetchFile(video));
+
+    await ffmpeg.run('-i', 'src.mp4', '-t', '2.5', '-ss', '2.0', '-f', 'gif', 'out.gif');
+
+    const data = ffmpeg.FS("readFile", "out.gif");
+
+    const url = URL.createObjectURL(new Blob([data.buffer], { type: "image/gif" }));
+    setGif(url);
+  }
+
+  return ready ? (
+    <div className="App">
+      {video && <video controls width="250" src={URL.createObjectURL(video)}></video>}
+      <input type="file" onChange={(e) => setVideo(e.target.files?.item(0))}/>
+
+      <button onClick={convertToGif}>Convert to Gif</button>
+
+      {gif && <img src={gif} width="250" />}
+    </div>
+  ) : (
+    <p>Loading...</p>
+  );
 }
